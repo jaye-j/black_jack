8e4e9a9d020f46dcbe2b0c6761482d911f4b1910
jaye-j <jacobrobertjensen@gmail.com>
2020-02-09 18:57:27 -0600

    made table and starting deck

---
+++ blackjack.js
@@ -0,0 +1,4 @@
+
+
+
+deck = [{suit: "Diamonds", value: 2, imageURL: "JPEG/2D.jpg"}, {suit: "Diamonds", value: 3, imageURL: "JPEG/3D.jpg"}, {suit: "Diamonds", value: 4, imageURL: "JPEG/4D.jpg"}, {suit: "Diamonds", value: 5, imageURL: "JPEG/5D.jpg"}, {suit: "Diamonds", value: 6, imageURL: "JPEG/6D.jpg"}, ]
