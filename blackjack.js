39b0978371dd2f2bf9dec16c5a9c099271322ecc
jaye-j <jacobrobertjensen@gmail.com>
2020-02-10 16:26:32 -0600

    needs dealer hit logic

--- blackjack.js
+++ blackjack.js
@@ -1,6 +1,4 @@
 
-
-
 let deck = [{suit: "Diamonds", value: 2, imageURL: "JPEG/2D.jpg"},
 {suit: "Diamonds", value: 3, imageURL: "JPEG/3D.jpg"},
 {suit: "Diamonds", value: 4, imageURL: "JPEG/4D.jpg"},
@@ -10,7 +8,7 @@ let deck = [{suit: "Diamonds", value: 2, imageURL: "JPEG/2D.jpg"},
 {suit: "Diamonds", value: 8, imageURL: "JPEG/8D.jpg"},
 {suit: "Diamonds", value: 9, imageURL: "JPEG/9D.jpg"},
 {suit: "Diamonds", value: 10, imageURL: "JPEG/10D.jpg"},
-{suit: "Diamonds", value: [11, 1], imageURL: "JPEG/AD.jpg"},
+{suit: "Diamonds", value: 11, imageURL: "JPEG/AD.jpg"},
 {suit: "Diamonds", value: 10, imageURL: "JPEG/JD.jpg"},
 {suit: "Diamonds", value: 10, imageURL: "JPEG/QD.jpg"},
 {suit: "Diamonds", value: 10, imageURL: "JPEG/KD.jpg"},
@@ -23,7 +21,7 @@ let deck = [{suit: "Diamonds", value: 2, imageURL: "JPEG/2D.jpg"},
 {suit: "Hearts", value: 8, imageURL: "JPEG/8H.jpg"},
 {suit: "Hearts", value: 9, imageURL: "JPEG/9H.jpg"},
 {suit: "Hearts", value: 10, imageURL: "JPEG/10H.jpg"},
-{suit: "Hearts", value: [11, 1], imageURL: "JPEG/AH.jpg"},
+{suit: "Hearts", value: 11, imageURL: "JPEG/AH.jpg"},
 {suit: "Hearts", value: 10, imageURL: "JPEG/JH.jpg"},
 {suit: "Hearts", value: 10, imageURL: "JPEG/QH.jpg"},
 {suit: "Hearts", value: 10, imageURL: "JPEG/KH.jpg"},
@@ -36,7 +34,7 @@ let deck = [{suit: "Diamonds", value: 2, imageURL: "JPEG/2D.jpg"},
 {suit: "Clubs", value: 8, imageURL: "JPEG/8C.jpg"},
 {suit: "Clubs", value: 9, imageURL: "JPEG/9C.jpg"},
 {suit: "Clubs", value: 10, imageURL: "JPEG/10C.jpg"},
-{suit: "Clubs", value: [11, 1], imageURL: "JPEG/AC.jpg"},
+{suit: "Clubs", value: 11, imageURL: "JPEG/AC.jpg"},
 {suit: "Clubs", value: 10, imageURL: "JPEG/JC.jpg"},
 {suit: "Clubs", value: 10, imageURL: "JPEG/QC.jpg"},
 {suit: "Clubs", value: 10, imageURL: "JPEG/KC.jpg"},
@@ -49,19 +47,56 @@ let deck = [{suit: "Diamonds", value: 2, imageURL: "JPEG/2D.jpg"},
 {suit: "Spades", value: 8, imageURL: "JPEG/8S.jpg"},
 {suit: "Spades", value: 9, imageURL: "JPEG/9S.jpg"},
 {suit: "Spades", value: 10, imageURL: "JPEG/10S.jpg"},
-{suit: "Spades", value: [11, 1], imageURL: "JPEG/AS.jpg"},
+{suit: "Spades", value: 11, imageURL: "JPEG/AS.jpg"},
 {suit: "Spades", value: 10, imageURL: "JPEG/JS.jpg"},
 {suit: "Spades", value: 10, imageURL: "JPEG/QS.jpg"},
 {suit: "Spades", value: 10, imageURL: "JPEG/KS.jpg"}]
 
+let playerValueDisplayTotal = document.getElementById("player");
+let dealerValueDisplayTotal = document.getElementById("dealer");
 let dealerValue = 0;
 let playerValue = 0;
+let playerAceCount = 0;
+let dealerAceCount = 0;
 dealerHandValue = [];
 playerHandValue = [];
 
 console.log(deck);
 
 //functions
+function displayTie() {
+    let overlayDiv = document.createElement('div');
+    overlayDiv.setAttribute('id', 'overlay');
+    overlayDiv.setAttribute(
+        'style',
+        "position: absolute; width: 100%; height: 570px; background-image: linear-gradient(to right, rgba(255,255,0,0), rgba(255,255,0,1), rgba(255,255,0,0)); top: 190px; text-align: center; font-size: 72px; opacity: .8; display: flex; justify-content: center; align-items: center; font-size: 120px; font-family: 'Bree Serif', serif;"
+    );
+    overlayDiv.textContent = 'Tie game! Press deal to play again...';
+    document.body.appendChild(overlayDiv);
+};
+
+function displayWin() {
+    let overlayDiv = document.createElement('div');
+    overlayDiv.setAttribute('id', 'overlay');
+    overlayDiv.setAttribute(
+        'style',
+        "position: absolute; width: 100%; height: 570px; background-image: linear-gradient(to right, rgba(255,255,0,0), rgba(255,255,0,1), rgba(255,255,0,0)); top: 190px; text-align: center; font-size: 72px; opacity: .8; display: flex; justify-content: center; align-items: center; font-size: 120px; font-family: 'Bree Serif', serif;"
+    );
+    overlayDiv.textContent = 'Congratulations, you win! Press deal to play again...';
+    document.body.appendChild(overlayDiv);
+};
+
+function displayLoss() {
+    let overlayDiv = document.createElement('div');
+    overlayDiv.setAttribute('id', 'overlay');
+    overlayDiv.setAttribute(
+        'style',
+        "position: absolute; width: 100%; height: 570px; background-image: linear-gradient(to right, rgba(255,255,0,0), rgba(255,255,0,1), rgba(255,255,0,0)); top: 190px; text-align: center; font-size: 72px; opacity: .7; display: flex; justify-content: center; align-items: center; font-size: 120px; font-family: 'Bree Serif', serif;"
+    );
+    overlayDiv.textContent = 'You lose! Press deal to play again...';
+    document.body.appendChild(overlayDiv);
+};
+
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
     var j = Math.floor(Math.random() * (i + 1));
@@ -70,47 +105,97 @@ function shuffleArray(array) {
     array[j] = temp;
     }
     return array;
-    };
+};
 
 function addingInitialValues() {
     dealerValue = dealerHandValue[0] + dealerHandValue[1];
     playerValue = playerHandValue[0] + playerHandValue[1];
-}
+};
 
 function addingPlayerHitValues() {
     let hitValue = playerValue + playerHandValue[0]
     playerValue = hitValue
-}
+    if (playerHandValue[0] == 11 && playerValue >= 11){
+        playerValue = playerValue - 10;
+    };
+};
+
+function displayPlayerValue() {
+    playerValueDisplayTotal.innerHTML = "Player: " + playerValue;
+};
+
+function displayDealerValue() {
+    dealerValueDisplayTotal.innerHTML = "Dealer: " + dealerValue;
+};
+
 
 function deal() {
     shuffleArray(deck);
+
+    
+
+    dealerHandValue = [];
+    playerHandValue = [];
+    let dealer1 = document.getElementById('dealer-hand');
+    let player1 = document.getElementById('player-hand');
+    dealer1.innerHTML = '';
+    player1.innerHTML = '';
     let deal1 = document.createElement("IMG");
         deal1.src = deck[0].imageURL;
         deal1.className = "hand"
-        dealerHandValue.push(deck[0].value);
+        if (deck[0].value == 11){
+            dealerHandValue.push(deck[0].value);
+            dealerAceCount++;
+        }else{
+            dealerHandValue.push(deck[0].value);
+        };
         deck.shift();
     let deal2 = document.createElement("IMG");
         deal2.src = deck[0].imageURL;
         deal2.className = "hand"
-        dealerHandValue.push(deck[0].value);
+        if (deck[0].value == 11){
+            dealerHandValue.push(deck[0].value);
+            dealerAceCount++;
+        }else{
+            dealerHandValue.push(deck[0].value);
+        };
         deck.shift();
-    let dealer1 = document.getElementById("dealer-hand");
             dealer1.append(deal1, deal2);
     console.log(deal1);
 
     let play1 = document.createElement("IMG");
         play1.src = deck[0].imageURL;
         play1.className = "hand"
-        playerHandValue.push(deck[0].value);
+        if (deck[0].value == 11){
+            playerHandValue.push(deck[0].value);
+            playerAceCount++;
+        }else{
+            playerHandValue.push(deck[0].value);
+        };
         deck.shift();
     let play2 = document.createElement("IMG");
         play2.src = deck[0].imageURL;
         play2.className = "hand"
-        playerHandValue.push(deck[0].value);
+        if (deck[0].value == 11){
+            playerHandValue.push(deck[0].value);
+            playerAceCount++;
+        }else{
+            playerHandValue.push(deck[0].value);
+        };
         deck.shift();
-    let player1 = document.getElementById("player-hand");
             player1.append(play1, play2);
     addingInitialValues();
+    displayPlayerValue();
+    displayDealerValue();
+
+    try{
+        let t = document.getElementById('overlay');
+    
+        t.setAttribute("class", "overlayhidden")
+        }
+        catch{}
+
+        console.log("here");
 };
 
 function hit() {
@@ -124,26 +209,45 @@ function hit() {
             playerhit.append(playhit);
     console.log(playhit);
     addingPlayerHitValues();
+    displayPlayerValue();
 };
 
-function stand() {
+function checkValue() {
+    if (playerValue <= 21 && dealerValue <= 21) {
+      //dealer method
+      if (playerValue > dealerValue) {
+        displayWin();
+      } else if (dealerValue > playerValue) {
+        displayLoss();
+      } else {
+        displayTie();
+      }
+    } else if (playerValue > 21) {
+      displayLoss();
+    } else if (dealerValue > 21) {
+      displayWin();
+    };
+  };
 
-};
+function endGame() {
+    if (playerValue >= 21) {
+      checkValue();
+    };
+  };  
 //////
 
 ///event listeners
 document.getElementById("deal-button").addEventListener("click", function(e){
     deal();
-    console.log(dealerValue);
-    console.log(playerValue);
+    endGame();
 
 });
 
 document.getElementById("hit-button").addEventListener("click", function(e){
     hit();
-    console.log(playerValue)
+    endGame();
 });
 
 document.getElementById("stand-button").addEventListener("click", function(e){
-    stand();
+    checkValue()
 })
